a277091630020a0e6607665e0bbf57da2a084dbe
ApiratChiwakanit <apirat.c@roundtwosolutions.com>
2019-04-03 10:34:27 +0700

    variable locales

--- pages/home.js
+++ pages/home.js
@@ -6,6 +6,7 @@ const home = () => {
         <div>
             <h1>{locale.t('hello')}</h1>
             <h2>{locale.t('greeting')}</h2>
+            <h2>{locale.t('name', {name: 'จอห์น'})}</h2>
         </div>
     )
 }
